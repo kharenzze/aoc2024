6cb3206080a40beee4d451e6e5f1a0359aa318eb
Paolo Ráez <str237@gmail.com>
2024-12-04 07:56:13 +0100

    Day 3 P2

--- src/e3.rs
+++ src/e3.rs
@@ -26,7 +26,29 @@ fn initial(input: Input) -> Output1 {
 }
 
 fn extra(input: Input) -> Output2 {
-  unimplemented!()
+  let re = Regex::new(r"(mul|do|don't)\(((\d{1,3}),(\d{1,3}))?\)").unwrap();
+  let mut score = 0;
+  let mut computing = true;
+  for cap in re.captures_iter(&input) {
+    let op = &cap[1];
+    match op {
+      "mul" => {
+        if computing {
+          let a = cap[3].parse::<i64>().unwrap();
+          let b = cap[4].parse::<i64>().unwrap();
+          score += a * b;
+        }
+      }
+      "do" => {
+        computing = true;
+      }
+      "don't" => {
+        computing = false;
+      }
+      _ => unreachable!(),
+    }
+  }
+  score
 }
 
 pub fn solve(part: usize) {
@@ -52,8 +74,9 @@ mod tests {
 
   #[test]
   fn two() {
-    let input = read_data(true);
+    let input =
+      "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string();
     let score = extra(input);
-    assert_eq!(score, 13)
+    assert_eq!(score, 48)
   }
 }
