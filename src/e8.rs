4006f6e65bd585085d5efe21c68911ffaa12292f
Paolo Ráez <str237@gmail.com>
2024-12-13 19:01:58 +0100

    Day 8 P1

--- src/e8.rs
+++ src/e8.rs
@@ -1,5 +1,8 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+
+use crate::point::{Bounds, Point};
 
 type Input = Vec<String>;
 type Output1 = i64;
@@ -17,7 +20,8 @@ fn read_data(is_test: bool) -> Input {
 }
 
 fn initial(input: Input) -> Output1 {
-  unimplemented!()
+  let game = Game::new(input);
+  game.solve()
 }
 
 fn extra(input: Input) -> Output2 {
@@ -34,6 +38,61 @@ pub fn solve(part: usize) {
   println!("{score}")
 }
 
+#[derive(Debug, Clone)]
+struct Game {
+  grid: Vec<Vec<char>>,
+  pos_map: HashMap<char, HashSet<Point>>,
+  bounds: Bounds,
+}
+
+impl Game {
+  fn new(input: Input) -> Self {
+    let grid: Vec<Vec<char>> = input.iter().map(|l| l.chars().collect()).collect();
+    let mut pos_map: HashMap<char, HashSet<Point>> = HashMap::new();
+    for (y, row) in grid.iter().enumerate() {
+      for (x, &c) in row.iter().enumerate() {
+        if c != '.' {
+          let point = Point::new(x as i64, y as i64);
+          pos_map.entry(c).or_default().insert(point);
+        }
+      }
+    }
+    let y = grid.len();
+    let x = grid[0].len();
+    let bounds = Bounds::from_dims(x, y);
+
+    Self {
+      grid,
+      pos_map,
+      bounds,
+    }
+  }
+
+  fn solve(&self) -> i64 {
+    let mut antenna_pos: HashSet<Point> = HashSet::new();
+    for (_, points) in self.pos_map.iter() {
+      let n = points.len();
+      if n < 2 {
+        continue;
+      }
+      for (i, &a) in points.iter().enumerate().take(n - 1) {
+        for &b in points.iter().skip(i + 1) {
+          let diff = (b - a);
+          let antenna = a - diff;
+          if self.bounds.check(&antenna) {
+            antenna_pos.insert(antenna);
+          }
+          let antenna = b + diff;
+          if self.bounds.check(&antenna) {
+            antenna_pos.insert(antenna);
+          }
+        }
+      }
+    }
+    antenna_pos.len() as i64
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -42,7 +101,7 @@ mod tests {
   fn simple() {
     let input = read_data(true);
     let score = initial(input);
-    assert_eq!(score, 13)
+    assert_eq!(score, 14)
   }
 
   #[test]
